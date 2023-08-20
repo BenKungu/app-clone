8b4221c9e24af2f7b73435d4a57556447e4e2206
BenKungu <benwaweru2013@gmail.com>
2023-08-20 21:51:15 +0300

    axios installation

--- src/pages/services/Request.jsx
+++ src/pages/services/Request.jsx
@@ -1,1 +1,18 @@
 import { axiosInstance } from "./TokenService"
+
+class RequestService {
+
+    createAppointment = (data) => {
+        return axiosInstance.post("appointment", data);
+    }
+
+    getAppointments = () => {
+        return axiosInstance.get("appointments");
+    }
+
+    getSlots = (dates) => {
+        return axiosInstance.get("slots?start_date=" + dates);
+    }
+}
+
+export default new RequestService();
