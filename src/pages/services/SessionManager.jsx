3236bdc8c445072199f46462080236cf41a93fb2
BenKungu <benwaweru2013@gmail.com>
2023-09-24 14:40:00 +0300

    Unified localstorage

--- src/pages/services/SessionManager.jsx
+++ src/pages/services/SessionManager.jsx
@@ -1,10 +1,8 @@
 
 const SESSION_KEY = 'hisaAppSession';
-// const AUTHENTICATION_KEY = 'isAuthenticated';
 
 export const setSession = (data) => {
     localStorage.setItem(SESSION_KEY, JSON.stringify(data));
-    // sessionStorage.setItem(AUTHENTICATION_KEY, data.isAuthenticated);
 };
 
 export const getSession = () => {
